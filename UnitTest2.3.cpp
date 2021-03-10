eecb1443f1d3e500060d44512ee2ccfdb1361894
Feynom <53947611+Feynom@users.noreply.github.com>
2021-03-10 17:35:18 +0200

    Add files via upload

---
+++ UnitTest2.3.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab2.3/Lab2.3/Vector3D.h"
+#include "../Lab2.3/Lab2.3/Vector3D.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest23
+{
+	TEST_CLASS(UnitTest23)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Vector3D A(1, 2, 3), B(4, 5, 6);
+			//A * B	
+			Assert::AreEqual(A * B, 32.);
+		}
+	};
+}
